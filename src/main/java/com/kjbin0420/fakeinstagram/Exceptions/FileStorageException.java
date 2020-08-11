d5c1fb68d43a7d16b002497f605bcb0b13c63ba7
kjbin0420 <kjbin0420@naver.com>
2020-08-11 19:07:11 +0900

    user profile Image

---
+++ src/main/java/com/kjbin0420/fakeinstagram/Exceptions/FileStorageException.java
@@ -0,0 +1,9 @@
+package com.kjbin0420.fakeinstagram.Exceptions;
+
+public class FileStorageException extends RuntimeException{
+    private static final long serialVersionUID = 6712417769526744426L;
+
+    public FileStorageException(String fileName) {
+        super("Could not store file!" + fileName);
+    }
+}
--- src/main/java/com/kjbin0420/fakeinstagram/Payload/Request/RegisterRequest.java
+++ src/main/java/com/kjbin0420/fakeinstagram/Payload/Request/RegisterRequest.java
@@ -1,14 +1,17 @@
 package com.kjbin0420.fakeinstagram.Payload.Request;
 
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Email;
+import java.util.Optional;
 
 @Getter
 public class RegisterRequest {
     String userId;
     String userPw;
     String userName;
+    MultipartFile profileImage;
 
     @Email
     String userEmail;
